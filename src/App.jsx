5062a530f2d08886d1aedcb0cb752641089c77d7
Davy Nyakinyua <davynyakinyua@gmail.com>
2025-10-09 11:05:19 +0300

    Remove vite default code and add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import './reset.css'
+import Head from './components/header/header.jsx'
 
 function App() {
-  
+    return (
+      <Head />
+    );
 }
 
 export default App
---
+++ src/components/header/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css'
+
+function Head (){
+    return (
+        <>
+            <h1>Resume Creator</h1>
+            <p>Fill in your details below to build your resume.</p>
+        </>
+    );
+}
+
+
+export default Head
